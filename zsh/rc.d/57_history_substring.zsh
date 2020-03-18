30793c361ac033c629338e4b01aef9f5657fd0c9
Brian Leppez <brian.leppez@ey.com>
2020-03-17 22:03:41 -0500

    Installed Dracula

--- zsh/rc.d/57_history_substring.zsh
+++ zsh/rc.d/57_history_substring.zsh
@@ -2,5 +2,17 @@
 source "${ZDOTDIR}/plugins/history-substring-search/zsh-history-substring-search.zsh"
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=green,fg=white'
 HISTORY_SUBSTRING_SEARCH_FUZZY=1
-bindkey "${key[Up]}"   history-substring-search-up
-bindkey "${key[Down]}" history-substring-search-down
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+# Bind up and down keys
+zmodload -F zsh/terminfo +p:terminfo
+if [[ -n ${terminfo[kcuu1]} && -n ${terminfo[kcud1]} ]]; then
+  bindkey ${terminfo[kcuu1]} history-substring-search-up
+  bindkey ${terminfo[kcud1]} history-substring-search-down
+fi
+  
+bindkey '^P' history-substring-search-up
+bindkey '^N' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
