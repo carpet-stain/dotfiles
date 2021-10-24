b76912607b14c9c4113c30d04100ce86a6a76a53
Brian Leppez <brian.leppez@ey.com>
2021-10-23 19:34:21 -0500

    Added base ext4 arch install - testing

---
+++ arch/install.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+#
+# Arch Linux installation
+#
+# Bootable USB:
+# - [Download](https://archlinux.org/download/) ISO and GPG files
+# - Verify the ISO file: `$ pacman-key -v archlinux-<version>-dual.iso.sig`
+# - Create a bootable USB with: `# dd if=archlinux*.iso of=/dev/sdX && sync`
+#
+# UEFI setup:
+#
+# - Set boot mode to UEFI, disable Legacy mode entirely.
+# - Temporarily disable Secure Boot.
+# - Make sure a strong UEFI administrator password is set.
+# - Delete preloaded OEM keys for Secure Boot, allow custom ones.
+# - Set SATA operation to AHCI mode.
+#
+# Run installation:
+#
+# - Connect to wifi via: `# iwctl station wlan0 connect WIFI-NETWORK`
+# - Run: `# bash <(curl -sL https://git.io/maximbaz-install)`
+
+set -uo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+
+exec 1> >(tee "stdout.log")
+exec 2> >(tee "stderr.log" >&2)
+
+export SNAP_PAC_SKIP=y
+
+# Dialog
+BACKTITLE="Arch Linux installation"
+
+get_input() {
+    title="$1"
+    description="$2"
+
+    input=$(dialog --clear --stdout --backtitle "$BACKTITLE" --title "$title" --inputbox "$description" 0 0)
+    echo "$input"
+}
+
+get_password() {
+    title="$1"
+    description="$2"
+
+    init_pass=$(dialog --clear --stdout --backtitle "$BACKTITLE" --title "$title" --passwordbox "$description" 0 0)
+    : ${init_pass:?"password cannot be empty"}
+
+    test_pass=$(dialog --clear --stdout --backtitle "$BACKTITLE" --title "$title" --passwordbox "$description again" 0 0)
+    if [[ "$init_pass" != "$test_pass" ]]; then
+        echo "Passwords did not match" >&2
+        exit 1
+    fi
+    echo $init_pass
+}
+
+get_choice() {
+    title="$1"
+    description="$2"
+    shift 2
+    options=("$@")
+    dialog --clear --stdout --backtitle "$BACKTITLE" --title "$title" --menu "$description" 0 0 0 "${options[@]}"
+}
+
+echo -e "\n### Checking UEFI boot mode"
+if [ ! -f /sys/firmware/efi/fw_platform_size ]; then
+    echo >&2 "You must boot in UEFI mode to continue"
+    exit 2
+fi
+
+echo -e "\n### Setting up clock"
+timedatectl set-ntp true
+hwclock --systohc --utc
+
+echo -e "\n### Installing additional tools"
+pacman -Sy --noconfirm --needed terminus-font dialog
+
+echo -e "\n### HiDPI screens"
+noyes=("Yes" "The font is too small" "No" "The font size is just fine")
+hidpi=$(get_choice "Font size" "Is your screen HiDPI?" "${noyes[@]}") || exit 1
+clear
+[[ "$hidpi" == "Yes" ]] && font="ter-132n" || font="ter-716n"
+setfont "$font"
+
+hostname=$(get_input "Hostname" "Enter hostname") || exit 1
+clear
+: ${hostname:?"hostname cannot be empty"}
+
+user=$(get_input "User" "Enter username") || exit 1
+clear
+: ${user:?"user cannot be empty"}
+
+password=$(get_password "User" "Enter password") || exit 1
+clear
+: ${password:?"password cannot be empty"}
+
+devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)
+device=$(get_choice "Installation" "Select installation disk" "${devicelist[@]}") || exit 1
+clear
+
+### Setup the disk and partitions ###
+swap_size=$(free --mebi | awk '/Mem:/ {print $2}')
+swap_end=$(( $swap_size + 129 + 1 ))MiB
+
+parted --script "${device}" -- mklabel gpt \
+  mkpart ESP fat32 1Mib 129MiB \
+  set 1 boot on \
+  mkpart primary linux-swap 129MiB ${swap_end} \
+  mkpart primary ext4 ${swap_end} 100%
+
+# Simple globbing was not enough as on one device I needed to match /dev/mmcblk0p1
+# but not /dev/mmcblk0boot1 while being able to match /dev/sda1 on other devices
+part_boot="$(ls ${device}* | grep -E "^${device}p?1$")"
+part_swap="$(ls ${device}* | grep -E "^${device}p?2$")"
+part_root="$(ls ${device}* | grep -E "^${device}p?3$")"
+
+wipefs "${part_boot}"
+wipefs "${part_swap}"
+wipefs "${part_root}"
+
+mkfs.vfat -F32 "${part_boot}"
+mkswap "${part_swap}"
+mkfs.f2fs -f "${part_root}"
+
+swapon "${part_swap}"
+mount "${part_root}" /mnt
+mkdir /mnt/boot
+mount "${part_boot}" /mnt/boot
+
+echo -e "\n### Setting up fastest mirrors"
+reflector --latest 30 --sort rate --save /etc/pacman.d/mirrorlist
+
+pacstrap /mnt base base-devel zsh efibootmgr man-db man-pages intel-ucode nftables iw iwd git cmake jq sddm firefox plasma-desktop plasma-wayland-session bluez bluez-utils dolphin firefox kate konsole plasma-nm plasma-pa
+genfstab -t PARTUUID /mnt >> /mnt/etc/fstab
+echo "${hostname}" > /mnt/etc/hostname
+
+arch-chroot /mnt bootctl install
+
+cat <<EOF > /mnt/boot/loader/loader.conf
+default arch
+EOF
+
+cat <<EOF > /mnt/boot/loader/entries/arch.conf
+title    Arch Linux
+linux    /vmlinuz-linux
+initrd   /intel-ucode.img
+initrd   /initramfs-linux.img
+options  root=PARTUUID=$(blkid -s PARTUUID -o value "$part_root") rw
+EOF
+
+echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
+
+arch-chroot /mnt useradd -mU -s /usr/bin/zsh -G wheel,uucp,video,audio,storage,input "$user"
+arch-chroot /mnt chsh -s /usr/bin/zsh
+
+echo "$user:$password" | chpasswd --root /mnt
+echo "root:$password" | chpasswd --root /mnt
+
+echo -e "\n### Reboot now, and after power off remember to unplug the installation USB"
+umount -R /mnt
