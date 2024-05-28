0568f7cfb969be07ce35812f991340c114f2bac3
brian.leppez <brian.leppez>
2024-05-28 12:34:00 -0400

    added deploy script

---
+++ nixos/vm/install.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# Arch Linux installation
+#
+# Bootable USB:
+# - [Download](https://archlinux.org/download/) ISO and GPG files
+# - Verify the ISO file: `$ pacman-key -v archlinux-<version>-dual.iso.sig`
+# - Create a bootable USB with: `# dd if=archlinux*.iso of=/dev/sdX && sync`
+#
+# UEFI setup:
+#
+# - Set boot mode to UEFI, disable Legacy mode entirely.
+# - Temporarily disable Secure Boot.
+# - Make sure a strong UEFI administrator password is set.
+# - Delete preloaded OEM keys for Secure Boot, allow custom ones.
+# - Set SATA operation to AHCI mode.
+#
+# Run installation:
+#
+# - Connect to wifi via: `# iwctl station wlan0 connect WIFI-NETWORK`
+# - Run: `# bash <(curl -sL https://t.ly/o4kG)`
+
+set -uo pipefail
+trap on_error ERR
+
+# Redirect output to files for easier debugging
+exec 1> >(tee "stdout.log")
+exec 2> >(tee "stderr.log" >&2)
+
+FLAKE="github:carpet-stain/dotfiles?dir=nixos/vm#nixos"
+
+on_error() {
+	ret=$?
+	echo "[$0] Error on line $LINENO: $BASH_COMMAND"
+	exit $ret
+}
+
+get_input() {
+	title="$1"
+	description="$2"
+
+	input=$(dialog --clear --stdout --backtitle "$BACKTITLE" --title "$title" --inputbox "$description" 0 0)
+	echo "$input"
+}
+
+get_password() {
+	title="$1"
+	description="$2"
+
+	init_pass=$(dialog --clear --stdout --backtitle "$BACKTITLE" --title "$title" --passwordbox "$description" 0 0)
+	test -z "$init_pass" && echo >&2 "password cannot be empty" && exit 1
+
+	test_pass=$(dialog --clear --stdout --backtitle "$BACKTITLE" --title "$title" --passwordbox "$description again" 0 0)
+	if [[ "$init_pass" != "$test_pass" ]]; then
+		echo "Passwords did not match" >&2
+		exit 1
+	fi
+	echo "$init_pass"
+}
+
+get_choice() {
+	title="$1"
+	description="$2"
+	shift 2
+	options=("$@")
+	dialog --clear --stdout --backtitle "$BACKTITLE" --title "$title" --menu "$description" 0 0 0 "${options[@]}"
+}
+
+nix-shell -p dialog
+
+# Ask which device to install ArchLinux on
+devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac | tr '\n' ' ')
+read -r -a devicelist <<<"$devicelist"
+DEVICE=$(get_choice "Installation" "Select installation disk" "${devicelist[@]}") || exit 1
+clear
