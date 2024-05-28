0a6d2f98a78c1b1b77c4ae2e8b72cfa28b11b26f
brian.leppez <brian.leppez>
2024-05-28 12:00:32 -0400

    added missing ;

--- nixos/vm/configuration.nix
+++ nixos/vm/configuration.nix
@@ -12,7 +12,7 @@ networking.wireless.enable = true;
 
 time.timeZone = "America/New_York"
 
-i18n.defaultLocale = "en_US.UTF-8"
+i18n.defaultLocale = "en_US.UTF-8";
 
 users.users.brian = {
     isNormalUser = true;
