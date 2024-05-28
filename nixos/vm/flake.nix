671f123e8e061cb5f0539423daa28297553cbcb0
brian.leppez <brian.leppez>
2024-05-28 11:23:22 -0400

    added disko flake

--- nixos/vm/flake.nix
+++ nixos/vm/flake.nix
@@ -1,19 +1,36 @@
 {
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    };
-    
-    outputs = inputs@{ self, nixpkgs, ... }: {
-        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-            system = "aarch64-linux"
-            modules = [
-                ./configuration.nix
-                {
-                    nix = {
-                        settings.experimental-features = [ "nix-command" "flakes" ];
-                    };
-                }
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, nixpkgs, disko, ... }:
+    let
+      # TODO: Adjust these values to your needs
+      system = "aarch64-linux";
+      hostName = "nixos";
+    in
+    {
+      nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./disk-config.nix
+          disko.nixosModules.disko
+          ({ pkgs, ... }: {
+            boot.loader = {
+              systemd-boot.enable = true;
+              efi.canTouchEfiVariables = true;
+            };
+            networking = { inherit hostName; };
+            services.openssh.enable = true;
+            environment.systemPackages = with pkgs; [
+              htop
+              git
+              neovim
             ];
-        };
+            system.stateVersion = "23.11";
+          })
+        ];
+      };
     };
 }
