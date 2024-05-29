8dddd1247e611a6a1f2452a8ae14ccee67d3609c
brian.leppez <brian.leppez>
2024-05-29 13:05:25 -0400

    brought in the disk config into the flake.nix file

--- nixos/vm/disk-config.nix
+++ nixos/vm/disk-config.nix
@@ -2,7 +2,7 @@
   disko.devices = {
     disk = {
       main = {
-        device = "/dev/nvme0n1";
+        device = "/dev/disk/by-id/some-disk-id";
         type = "disk";
         content = {
           type = "gpt";
--- nixos/vm/flake.nix
+++ nixos/vm/flake.nix
@@ -13,7 +13,38 @@
       nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
-          ./disk-config.nix
+            {
+                disko.devices = {
+                    disk = {
+                        main = {
+                            device = "/dev/nvme0n1";
+                            type = "disk";
+                            content = {
+                                type = "gpt";
+                                partitions = {
+                                    ESP = {
+                                        type = "EF00";
+                                        size = "500M";
+                                        content = {
+                                            type = "filesystem";
+                                            format = "vfat";
+                                            mountpoint = "/boot";
+                                        };
+                                    };
+                                    root = {
+                                        size = "100%";
+                                        content = {
+                                            type = "filesystem";
+                                            format = "ext4";
+                                            mountpoint = "/";
+                                        };
+                                    };
+                                };
+                            };
+                        };
+                    };
+                };
+            }
           disko.nixosModules.disko
           ({ pkgs, ... }: {
             boot.loader = {
