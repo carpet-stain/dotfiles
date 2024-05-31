89cd069b1dc238bdfe6c6812579ac191ec3d5cbc
brian.leppez <brian.leppez>
2024-05-31 14:31:25 -0400

    simplifying the flake

--- nixos/vm/flake.nix
+++ nixos/vm/flake.nix
@@ -52,22 +52,12 @@
               efi.canTouchEfiVariables = true;
             };
             networking = { inherit hostName; };
-            networking.wireless.enable = true;
-
-            time.timeZone = "America/New_York";
-
-            i18n.defaultLocale = "en_US.UTF-8";
 
             users.users.brian = {
                 isNormalUser = true;
                 extraGroups = [ "wheel" ];
                 password = "brian";
-                packages = with pkgs; [
-                    firefox
-                    tree
-                ];
             };
-            services.openssh.enable = true;
             environment.systemPackages = with pkgs; [
               htop
               git
