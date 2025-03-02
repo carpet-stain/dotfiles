17423fa4e409763a70cd96077358aa2bf882840e
Brian Leppez <brian@leppez>
2025-03-02 12:32:14 -0600

    added more files

---
+++ zsh/rc.d/setopt.zsh
@@ -0,0 +1,59 @@
+# +------------+
+# | NAVIGATION |
+# +------------+
+
+setopt AUTO_CD          # if the command is directory and cannot be executed, perform cd to this directory
+setopt AUTO_PUSHD       # Make cd push the old directory onto the directory stack
+setopt PUSHD_SILENT     # Do not print the directory stack after pushd or popd.
+setopt CORRECT_ALL      # try to correct the spelling of all arguments in a line
+setopt CDABLE_VARS      # Change directory to a path stored in a variable.
+
+# +---------+
+# | HISTORY |
+# +---------+
+
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicates before oldest commands
+setopt HIST_NO_STORE             # Don't store commands starting with a space in the history file
+setopt INC_APPEND_HISTORY_TIME   # history appends to existing file as soon as it's written
+setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
+setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
+setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
+setopt HIST_REDUCE_BLANKS        # trim multiple insignificant blanks in history
+setopt SHARE_HISTORY             # Share history among all sessions
+
+HISTSIZE=1000000
+SAVEHIST=$HISTSIZE
+
+# History: Use standard ISO 8601 timestamp.
+#   %F is equivalent to %Y-%m-%d
+#   %T is equivalent to %H:%M:%S (24-hours format)
+HISTTIMEFORMAT='[%F %T]'
+
+setopt EXTENDED_GLOB             # Enables advanced globbing features
+unsetopt FLOW_CONTROL            # disable annoying keys 
+setopt CLOBBER                   # allow > redirection to truncate existing files
+setopt MULTIOS                   # allows multiple input and output redirections 
+setopt BRACE_CCL                 # allow brace character class list expansion
+unsetopt BEEP                    # do not beep on errors
+unsetopt NOMATCH                 # try to avoid the 'zsh: no matches found...'
+unsetopt SHORT_LOOPS             # Disable short loop forms
+setopt INTERACTIVE_COMMENTS      # allow use of comments in interactive code
+setopt AUTO_PARAM_SLASH          # complete folders with / at end
+setopt LIST_TYPES                # mark type of completion suggestions
+setopt HASH_LIST_ALL             # whenever a command completion is attempted, make sure the entire command path is hashed first
+setopt COMPLETE_IN_WORD          # allow completion from within a word/phrase
+setopt ALWAYS_TO_END             # move cursor to the end of a completed word
+setopt LONG_LIST_JOBS            # display PID when suspending processes as well
+setopt AUTO_RESUME               # attempt to resume existing job before creating a new process
+setopt NOTIFY                    # report status of background jobs immediately
+setopt NO_HUP                    # Don't send SIGHUP to background processes when the shell exits
+setopt PUSHD_IGNORE_DUPS         # don't push the same dir twice
+setopt NO_SH_WORD_SPLIT          # use zsh style word splitting
+unsetopt RM_STAR_SILENT          # notify when rm is running with *
+setopt RM_STAR_WAIT              # wait for 10 seconds confirmation when running rm with *
+
+# a bit fancier than default
+PROMPT_EOL_MARK='%K{red} %k'
